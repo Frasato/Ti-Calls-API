84981aa74ebc7f4e83bce5bc82547ab095795012
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-15 00:20:08 -0300

    Init project using Spring Boot

---
+++ src/main/java/com/frasatodev/ticall/TicallApplication.java
@@ -0,0 +1,13 @@
+package com.frasatodev.ticall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frasatodev/ticall/TicallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frasatodev.ticall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
