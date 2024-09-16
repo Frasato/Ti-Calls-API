56b0d0786ebee6c273a491549e6df26ede17b80e
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-16 15:17:42 -0300

    Query to find user

--- src/main/java/com/frasatodev/ticall/repositories/UserRepository.java
+++ src/main/java/com/frasatodev/ticall/repositories/UserRepository.java
@@ -2,9 +2,17 @@ package com.frasatodev.ticall.repositories;
 
 import com.frasatodev.ticall.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query(value = "SELECT * FROM tb_users WHERE username= :username AND password= :password", nativeQuery = true)
+    Optional<User> findByUsernamePassword(@Param("username") String username, @Param("password") String password);
+
+}
