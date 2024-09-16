ef0adf96416bae7383f5fd20efd9257b8a246e73
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-16 16:58:20 -0300

    function to find all calls

--- src/main/java/com/frasatodev/ticall/services/CallService.java
+++ src/main/java/com/frasatodev/ticall/services/CallService.java
@@ -1,13 +1,20 @@
 package com.frasatodev.ticall.services;
 
+import com.frasatodev.ticall.models.Call;
 import com.frasatodev.ticall.repositories.CallRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CallService {
 
     @Autowired
     private CallRepository callRepository;
+
+    public List<Call> getAllCalls(){
+        return callRepository.findAll();
+    }
 
 }
