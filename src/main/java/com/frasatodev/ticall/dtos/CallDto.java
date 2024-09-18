a5da2851fb1515bbc3466578e019545addd9d2f6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-18 11:02:30 -0300

    Create call dto

---
+++ src/main/java/com/frasatodev/ticall/dtos/CallDto.java
@@ -0,0 +1,40 @@
+package com.frasatodev.ticall.dtos;
+
+public class CallDto {
+    private String title;
+    private String description;
+    private String sector;
+    private String whoCalled;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getWhoCalled() {
+        return whoCalled;
+    }
+
+    public void setWhoCalled(String whoCalled) {
+        this.whoCalled = whoCalled;
+    }
+}
