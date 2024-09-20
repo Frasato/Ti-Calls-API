5a2f238fc7fafd7946a132ea7b50a2997a521749
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-20 15:44:30 -0300

    Create Status DTO

---
+++ src/main/java/com/frasatodev/ticall/dtos/StatusDto.java
@@ -0,0 +1,14 @@
+package com.frasatodev.ticall.dtos;
+
+public class StatusDto {
+
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
